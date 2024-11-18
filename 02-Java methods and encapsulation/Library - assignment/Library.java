ea6b590b0ae8d5324f75993f29b3cc1beabc03f8
Gomer0319 <augom0919@gmail.com>
2024-11-18 15:06:41 +0800

    Improved input feature

--- 02-Java methods and encapsulation/Library - assignment/Library.java
+++ 02-Java methods and encapsulation/Library - assignment/Library.java
@@ -33,18 +33,26 @@ public class Library {
         System.out.println ("Enter author of the book: ");
         String inputAuthor = input.nextLine();
 
+        // Call default constructor
+        Book defaultBook = new Book ();
+
         // Initialize book object. Call the overloaded constructor if there are inputs, otherwise call the default constructor.
+        Book book;
         if (inputTitle.length() > 0 && inputAuthor.length() > 0) {
-            Book book = new Book (inputTitle, inputAuthor);
-
-            //call display method
-            book.display();
-        } else {
-            Book book = new Book ();
-
-            //call display method
-            book.display();
+            book = new Book (inputTitle, inputAuthor);
         }
+        else if (inputTitle.length() > 0) {
+            book = new Book (inputTitle, defaultBook.author); // else if title is given but not author
+        }
+        else if (inputAuthor.length() > 0) {
+            book = new Book (defaultBook.title, inputAuthor); // else if author is given but not title
+        }
+        else {
+            book = new Book ();  
+        }
+
+        //call display method
+        book.display();
 
         input.close();
     }
