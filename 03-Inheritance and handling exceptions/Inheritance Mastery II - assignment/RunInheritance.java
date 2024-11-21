285f3c4ced6d46a71bc13b82b7d04ef64d1289ef
Gomer0319 <augom0919@gmail.com>
2024-11-21 09:21:38 +0800

    Inheritance

--- 03-Inheritance and handling exceptions/Inheritance Mastery II - assignment/RunInheritance.java
+++ 03-Inheritance and handling exceptions/Inheritance Mastery II - assignment/RunInheritance.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 // Abstract class
 abstract class Animal {
-    void makeSound() {
+    public void makeSound() {
         System.out.println("Some generic animal sound.");
     }
 }
@@ -24,22 +24,71 @@ class Cat extends Animal {
     }
 }
 
+// Add 3 more classes: Bird, Parrot, and Penguin
+// Bird should inherit from Animal, and be the new parent class of Parrot and Penguin
+class Bird extends Animal {
+    @Override
+    public void makeSound() {
+        System.out.println("Chirp!");
+        super.makeSound();
+    }
+}
+
+// Parrot should inherit from Bird
+class Parrot extends Bird {
+    @Override
+    public void makeSound() {
+        System.out.println("I can mimic sounds!");
+        super.makeSound();
+    }
+}
+
+// Penguin should inherit from Bird
+class Penguin extends Bird {
+    @Override
+    public void makeSound() {
+        System.out.println("Squawk!");
+        super.makeSound();
+    }
+}
+
 // Main method
 public class RunInheritance {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
         // User input
-        System.out.println("Press D for Dog, or C for Cat");
+        System.out.println("Select from the Following: \nD - Dog \nC - Cat \nB - Bird \nP - Parrot \nG - Penguin");
         String choice = input.next();
 
         // Check user input
         if (choice.equalsIgnoreCase("D")) {
+            System.out.println("A dog makes a sound:");
             Dog dog = new Dog();
             dog.makeSound();
-        } else if (choice.equalsIgnoreCase("C")) {
+        } 
+        else if (choice.equalsIgnoreCase("C")) {
+            System.out.println("A cat makes a sound:");
             Cat cat = new Cat();
             cat.makeSound();
+        }
+        else if (choice.equalsIgnoreCase("B")) {
+            System.out.println("A bird makes a sound:");
+            Bird bird = new Bird();
+            bird.makeSound();
+        }
+        else if (choice.equalsIgnoreCase("P")) {
+            System.out.println("A parrot makes a sound:");
+            Parrot parrot = new Parrot();
+            parrot.makeSound();
+        }
+        else if (choice.equalsIgnoreCase("G")) {
+            System.out.println("A penguin makes a sound:");
+            Penguin penguin = new Penguin();
+            penguin.makeSound();
+        } 
+        else {
+            System.out.println("Invalid choice.");
         }
 
         input.close();
